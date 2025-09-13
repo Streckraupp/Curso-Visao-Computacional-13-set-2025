6ad16e1e0ddfa62d959261644d5aa09cb7121498
Streckraupp <leonardo.streckraupp@gmail.com>
2025-09-13 10:52:59 -0300

    submit

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-image = cv2.imread("jardimdejacares.jpg")
+imagepath = "jardimdejacares.jpg"
+image = cv2.imread(imagepath)
+if image is None:
+	raise FileNotFoundError(f"Image {imagepath} not found or could not be loaded.")
 print(image.shape)
 rbg = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 plt.axis('off')
 plt.imshow(rbg, cmap=None)
 plt.show()
+
